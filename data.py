ed98eeee969d4f26eb0d5e4dd2fad99fe5996144
TrenchGoat45 <adam.cubakovic@gmail.com>
2025-02-27 13:37:24 -0500

    Second commit

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # File with constants
+URBAN_ROUTES_URL = 'https://cnt-0ae3f999-e279-41bc-be4d-b8484fed7c8c.containerhub.tripleten-services.com/'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER	= '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
